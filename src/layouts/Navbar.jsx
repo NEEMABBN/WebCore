c80d7c85f583c9bdeeada83a68b7b30dde534321
nima <nima.babanezhad.arimi@gmail.com>
2024-10-31 14:22:02 +0330

    SideMenu (50%)

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavbarItemsComponent from "../Components/NavbarItems/NavbarItemsComponent";
 import Images from "../Setting/Images";
 import { Link } from "react-router-dom";
 import { LuMenu } from "react-icons/lu";
+import SideMenu from "./SideMenu";
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (isMenuOpen && !event.target.closest(".menu")) {
+        setIsMenuOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isMenuOpen]);
+
+  const handleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
   return (
     <div className="w-[95%] mx-auto flex items-center justify-between fixed top-4 rounded-2xl bg-Secoundray px-5 py-2 z-10 shadow-lg">
-      <LuMenu className="md:hidden block text-white text-4xl" />
+      <LuMenu
+        onClick={handleMenu}
+        className="md:hidden block text-white text-4xl"
+      />
       <div className="md:flex items-center gap-9 hidden">
         <Link to="/">
           <img src={Images.Logo} alt="WebCore Logo" className="w-[35px]" />
@@ -20,6 +41,7 @@ export default function Navbar() {
         alt="WebCore Logo"
         className="md:hidden block w-[35px]"
       />
+      <SideMenu isMenuOpen={isMenuOpen} className="menu" />
     </div>
   );
 }
---
+++ src/layouts/SideMenu.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { FaXmark } from "react-icons/fa6";
+import { NavLink } from "react-router-dom";
+
+export default function SideMenu({ isMenuOpen }) {
+  return (
+    <div
+      className={`absolute top-0 ${
+        isMenuOpen ? "right-0" : "right-[-15rem]"
+      } md:hidden flex flex-col h-screen bg-[#2E2F36] w-[60%] rounded-2xl transition-all duration-300 py-7 px-5 gap-9`}
+    >
+      <div className="w-full flex items-center justify-between">
+        <FaXmark className="text-white" />
+        <button className="text-white">FA</button>
+      </div>
+
+      <ul className="w-full flex flex-col items-start gap-5">
+        <li className="">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `text-nowrap ${isActive ? "text-Primary" : "text-white"}`
+            }
+          >
+            صفحه اصلی
+          </NavLink>
+        </li>
+        <li className="">
+          <NavLink
+            to="/somePage"
+            className={({ isActive }) =>
+              `text-nowrap ${isActive ? "text-Primary" : "text-white"}`
+            }
+          >
+            محصولات
+          </NavLink>
+        </li>
+        <li className="">
+          <NavLink
+            to="/somePage"
+            className={({ isActive }) =>
+              `text-nowrap ${isActive ? "text-Primary" : "text-white"}`
+            }
+          >
+            خدمات
+          </NavLink>
+        </li>
+        <li className="">
+          <NavLink
+            to="/somePage"
+            className={({ isActive }) =>
+              `text-nowrap ${isActive ? "text-Primary" : "text-white"}`
+            }
+          >
+            دوره‌های آموزشی
+          </NavLink>
+        </li>
+        <li className="">
+          <NavLink
+            to="/somePage"
+            className={({ isActive }) =>
+              `text-nowrap ${isActive ? "text-Primary" : "text-white"}`
+            }
+          >
+            همکارامون
+          </NavLink>
+        </li>
+        <li className="">
+          <NavLink
+            to="/somePage"
+            className={({ isActive }) =>
+              `text-nowrap ${isActive ? "text-Primary" : "text-white"}`
+            }
+          >
+            تماس با ما
+          </NavLink>
+        </li>
+        <li className="">
+          <NavLink
+            to="/somePage"
+            className={({ isActive }) =>
+              `text-nowrap ${isActive ? "text-Primary" : "text-white"}`
+            }
+          >
+            بلاگ
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
